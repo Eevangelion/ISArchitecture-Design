ae67a85c66b52e7ed517cd7b26eae4583d840b7b
Alexandra Berezikova <alexberezikova@gmail.com>
2025-09-26 07:56:34 +0300

    lexer + parser WIP

--- include/parsing/lexer.h
+++ include/parsing/lexer.h
@@ -1,4 +1,16 @@
 #ifndef HW1_PARSING_LEXER_H_
 #define HW1_PARSING_LEXER_H_
 
+#include <vector>
+#include <string>
+
+class lexer_t 
+{
+public:
+    lexer_t();
+    ~lexer_t();
+
+    std::vector<std::string> tokenize(std::string const& input) const;
+};
+
 #endif
