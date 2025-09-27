53b16217be96ddf3e588003ca09a2e903fe5ea4a
Ivan Platonov <platonov.ivan347@gmail.com>
2025-09-27 15:18:25 +0300

    move tests

--- test/command/test_pwd.cpp
+++
@@ -1,7 +0,0 @@
-#include <command/pwd.h>
-#include <catch2/catch_test_macros.hpp>
-
-TEST_CASE("Prints current directory") {
-    pwd_command cmd;
-    REQUIRE(cmd.process() == std::filesystem::current_path());
-}
