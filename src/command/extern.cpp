06068da2c7c21f3a0370253b88f1b619c44aaaa3
Alexandra Berezikova <alexberezikova@gmail.com>
2025-09-27 17:22:22 +0300

    + extern command

---
+++ src/command/extern.cpp
@@ -0,0 +1,26 @@
+#include "command/extern.h"
+
+extern_command_t::extern_command_t(std::string const& command_name, std::vector<std::string> const& arguments)
+    : command_t(arguments)
+    , command_name_(command_name)
+{
+}
+
+extern_command_t::~extern_command_t()
+{
+}
+
+std::string extern_command_t::process() const
+{
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command_name_.c_str(), "r"), pclose);
+    
+    if (!pipe)
+        throw std::runtime_error("popen() failed!");
+
+    while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr) 
+        result += buffer.data();
+
+    return result;
+}
