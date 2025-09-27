4a01c9e108ff9538f3924563659696eb0ef76493
Alexandra Berezikova <alexberezikova@gmail.com>
2025-09-27 15:13:46 +0300

    fix tests +1

--- tests/command/test_cat.cpp
+++ tests/command/test_cat.cpp
@@ -1,5 +1,9 @@
 #include "command/cat.h"
+
 #include <vector>
+#include <fstream>
+#include <sstream>
+
 #include <catch2/catch_test_macros.hpp>
 
 std::vector<std::string> filepaths = {"test/command/test_filedata/1.txt", "test/command/test_filedata/2"};
