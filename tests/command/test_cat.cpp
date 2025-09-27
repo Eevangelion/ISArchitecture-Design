3125e71971c625b79b9b9b5ce675ac3fa80865cd
Ivan Platonov <platonov.ivan347@gmail.com>
2025-09-27 15:19:00 +0300

    add tests

---
+++ tests/command/test_cat.cpp
@@ -0,0 +1,71 @@
+#include "command/cat.h"
+#include <vector>
+#include <catch2/catch_test_macros.hpp>
+
+std::vector<std::string> filepaths = {"test/command/test_filedata/1.txt", "test/command/test_filedata/2"};
+
+
+TEST_CASE("Reading single file from relative path"){
+    cat_command_t cmd;
+    cmd.add_argument(filepaths[0]);
+
+    std::ifstream in(filepaths[0]);
+    std::stringstream buf;
+    buf << in.rdbuf();
+    std::string test = buf.str();
+
+    REQUIRE(cmd.process() == test);
+}
+
+TEST_CASE("Reading multiple files from relative path"){
+    cat_command_t cmd;
+    std::stringstream buf;
+
+    for (std::string &path : filepaths){
+        cmd.add_argument(path);
+
+        std::ifstream in(path);
+        buf << in.rdbuf();
+        in.close();
+    }
+
+    std::string test = buf.str();
+
+    REQUIRE(cmd.process() == test);
+}
+
+TEST_CASE("Reading single file from absolute path"){
+    cat_command_t cmd;
+    std::string abs_path = std::filesystem::current_path().string() + '/' + filepaths[0];
+    cmd.add_argument(abs_path);
+
+    std::ifstream in(abs_path);
+    std::stringstream buf;
+    buf << in.rdbuf();
+    std::string test = buf.str();
+
+    REQUIRE(cmd.process() == test);
+}
+
+TEST_CASE("Reading multiple files from absolute path"){
+    cat_command_t cmd;
+    std::string abs_path;
+    std::stringstream buf;
+    for (std::string path : filepaths) {
+        abs_path = std::filesystem::current_path().string() + '/' + path; 
+        cmd.add_argument(abs_path);
+
+        std::ifstream in(path);
+        buf << in.rdbuf();
+    }
+
+    std::string test = buf.str();
+
+    REQUIRE(cmd.process() == test);
+}
+
+TEST_CASE("Reading from non-existent files"){
+    cat_command_t cmd;
+    cmd.add_argument("gibberish");
+    REQUIRE_THROWS_AS(cmd.process(), std::runtime_error);
+}
---
+++ tests/command/test_wc.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_test_macros.hpp>
+
+#include "command/wc.h"
+
+TEST_CASE("Wc: common file") 
+{
+    wc_command_t wc_com;
+
+}
+
+TEST_CASE("Wc: empty file") 
+{
+    wc_command_t wc_com;
+}
