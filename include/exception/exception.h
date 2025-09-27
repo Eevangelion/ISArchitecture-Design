23d5dd84e22433c556dcd627fb5f50ff8c9695ba
Alexandra Berezikova <alexberezikova@gmail.com>
2025-09-27 17:08:57 +0300

    + exception

---
+++ include/exception/exception.h
@@ -0,0 +1,11 @@
+#pragma  once
+
+#include <stdexcept>
+
+class exit_exception : public std::exception 
+{
+    const char* what() const noexcept 
+    {
+        return "Exiting the application.";
+    } 
+};
