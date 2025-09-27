234afabca2b022191726509571837e265c3ae20e
Alexandra Berezikova <alexberezikova@gmail.com>
2025-09-27 12:39:57 +0300

    Merge remote-tracking branch 'origin/hw1_3-controller' into hw1-simple-interpreter +1

--- include/pipeline_processor.h
+++ include/pipeline_processor.h
@@ -10,6 +10,6 @@
 class pipeline_processor_t {
 
 public:
-    std::string process_commands(std::vector<std::unique_ptr<command_t> > commands) {}
+    std::string process_commands(std::vector<std::unique_ptr<command_t>> const& commands) {}
 };
 #endif
